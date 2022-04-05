6ea0268e8de858797ac852bb44408be8857f5cfa
Hariprasanth <hari99ma@gmail.com.com>
2022-04-05 19:30:30 +0530

    maven

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * HARIPRASANTH!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hariprasanth" );
+    }
+}
